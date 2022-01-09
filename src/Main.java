c7369a94cea58138a173e290d1c1f277ae846ca0
Al01z <dampc.artur@gmail.com>
2022-01-09 09:22:49 +0100

    naprawa

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Witam");
-        System.out.println("Laboratorium 3 ");
+        System.out.println("Laboratorium 3");
     }
 }
